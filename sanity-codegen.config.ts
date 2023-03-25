acb699c7495de34a7f6c9c470309d9775c96dafa
Savin <savin@angelmario.eu>
2023-03-26 00:57:24 +0200

    Updated workflow

--- sanity-codegen.config.ts
+++ sanity-codegen.config.ts
@@ -2,7 +2,7 @@ import { SanityCodegenConfig } from "sanity-codegen";
 
 const config: SanityCodegenConfig = {
 	schemaPath: "./schemas",
-	outputPath: "../app/types/SanitySchema.ts",
+	outputPath: "./SanitySchema.ts",
 };
 
 export default config;
