18f7639489e577d913f0da3c36ae7d7b12455bb9
Savin Angel-Mario <savin@angelmario.eu>
2023-04-27 13:41:51 +0300

    Removed users

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,14 +2,12 @@ import business from "./business";
 import organisation from "./organisation";
 import country from "./country";
 import volunteeringProject from "./volunteering-project";
-import user from "./user";
 
 export const schemaTypes = [
 	business,
 	organisation,
 	country,
 	volunteeringProject,
-	user,
 ];
 
 export default schemaTypes;
